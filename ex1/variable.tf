fc0b5a9fe842d323d9ced656cc4df34157b1e444
Mallika <kmallika.malli@gmail.com>
2023-05-24 11:44:58 +0530

    changes in the syntax

--- ex1/variable.tf
+++ ex1/variable.tf
@@ -1,23 +1,19 @@
-variable "sample"
-{
+variable "sample"{
   default = 100
 }
 
-variable "boolean"
-{
+variable "boolean"{
   default = true
 }
 
-variable "sam_list"
-{
+variable "sam_list"{
   default = [
   100,
   true,
   "hello world",
   10.2]
 }
-variable "sam_map"
-{
+variable "sam_map"{
   bool = false
   num = 10
   str= "devops"
