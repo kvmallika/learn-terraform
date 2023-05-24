f75649219073c311b0463b9a6c35a95e517c8d44
Mallika <kmallika.malli@gmail.com>
2023-05-24 11:37:15 +0530

    creating variables and display the output

---
+++ ex1/output.tf
@@ -0,0 +1,16 @@
+output "sample1"
+{
+  value = var.sample
+}
+output "boolean1"
+{
+  value = var.boolean
+}
+output "sam_list1"
+{
+  value = var.sam_list[2]
+}
+output "sam_map1"
+{
+  value = var.sam_map[boolean]
+}
---
+++ ex1/variable.tf
@@ -0,0 +1,25 @@
+variable "sample"
+{
+  default = 100
+}
+
+variable "boolean"
+{
+  default = true
+}
+
+variable "sam_list"
+{
+  default = [
+  100,
+  true,
+  "hello world",
+  10.2]
+}
+variable "sam_map"
+{
+  bool = false
+  num = 10
+  str= "devops"
+
+}
